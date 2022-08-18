f2866b6250195f0e435534aded3b0da8598cfd53
Devpe007 <anddev007pe@gmail.com>
2022-08-17 22:55:50 -0300

    create InfoSection component

---
+++ src/components/InfoSection/InfoSection.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import {
+  InfoSec,
+  InfoRow,
+  InfoColumn,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  ImgWrapper,
+  Img,
+} from './InfoSection.elements';
+
+import {
+  Container,
+  Button,
+} from '../../globalStyles';
+
+export default function InfoSection({
+  primary,
+  lightBg,
+  imgStart,
+  lightTopLine,
+  lightTextDesc,
+  lightText,
+  buttonLabel,
+  description,
+  headline,
+  topLine,
+  img,
+  alt,
+  start,
+}) {
+  return (
+    <>
+      <InfoSec
+        lightBg={lightBg}
+      >
+        <Container>
+          <InfoRow
+            imgStart={imgStart}
+          >
+            <InfoColumn>
+              <TextWrapper>
+                <TopLine
+                  lightTopLine={lightTopLine}
+                >
+                  {topLine}
+                </TopLine>
+                <Heading
+                  lightText={lightText}
+                >
+                  {headline}
+                </Heading>
+                <Subtitle
+                  lightTextDesc={lightTextDesc}
+                >
+                  {description}
+                </Subtitle>
+                <Link
+                  to="/sign-up"
+                >
+                  <Button
+                    big
+                    fontBig
+                    primary={primary}
+                  >
+                    {buttonLabel}
+                  </Button>
+                </Link>
+              </TextWrapper>
+            </InfoColumn>
+            <InfoColumn>
+              <ImgWrapper
+                start={start}
+              >
+                <Img
+                  src={img}
+                  alt={alt}
+                />
+              </ImgWrapper>
+            </InfoColumn>
+          </InfoRow>
+        </Container>
+      </InfoSec>
+    </>
+  );
+};
