0e8302b090a6fa6f437f9a0493655f62af5c1d3f
Devpe007 <anddev007pe@gmail.com>
2022-08-22 07:07:50 -0300

    create Pricing component structure

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -8,6 +8,7 @@ import {
 } from './Data';
 
 import { InfoSection } from '../../components';
+import Pricing from '../../components/Pricing/Pricing';
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <InfoSection {...HomeObjOne} />
       <InfoSection {...HomeObjTwo} />
       <InfoSection {...HomeObjThree} />
+      <Pricing />
       <InfoSection {...HomeObjFour} />
     </>
   );
